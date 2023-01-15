9931dbcdbaab43beba1725e421f5f9ce9199383f
Felipe-Sarmento <felipeleaosarmento@gmail.com>
2023-01-15 10:58:16 -0300

    startup configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-red-800">
+        Hello world!
+      </h1>
+        <h1>
+        Hello world!
+      </h1>
+    </>
+  )
+}
